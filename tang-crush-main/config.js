64fe41cb7dc334537ad64f882f6f014b42b64713
ThankDatjj <datnqk8apascal@gmail.com>
2026-03-13 16:14:28 +0700

    Add files via upload

---
+++ tang-crush-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Linhh à!!',
+introDesc: `Người ta đẹp kiểu dịu dàng                          
+Còn em đẹp kiểu nhìn là muốn khen
+Hỏi sao anh cứ đứng xem
+Tại vì em đẹp… nên quên mất đường`,
+    btnIntro: 'hihi',
+    title: 'Em có cảm thấy mình là người con gái xinh nhất thế giới khummm 🤔',
+    desc: 'Hãy trả lời thật lòng nhá ',
+    btnYes: 'Có 😊',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Em là 1 cô gái thực tế, tôi đánh giá cao sự trung thực của em. Tôi cho em tự chấm điểm cho sự xinh đẹp của bản thân',
+    btnReply: 'Gửi cho anh',
+    reply: 'Vô cùng tận ∞ ∞ ∞ ∞ ∞ ∞ ∞',
+    mess: 'Anh biết trước là em sẽ nói thật mà',
+    messDesc: 'Thuii chúc Linhhh ngủ ngonnnnn 😊😊😊😊',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=hOJjOzzzstM&list=RDhOJjOzzzstM&start_radio=1'
+}
